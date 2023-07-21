e37731f462fecb959b07b287f17879ce0874ee37
Bustomi <bustomi024@gmail.com>
2023-07-21 09:54:55 +0700

    menghapus file yang nggak ke pakai

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -80,50 +80,6 @@ class AdminController extends Controller
     // return redirect(route('pelanggan'))->with('success','berhasil masuk data yang diinputkan!!');
     // }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     public function tambahPelanggan(){
         return view('admin.tpelanggan');
@@ -137,7 +93,7 @@ class AdminController extends Controller
 
         // foreach ($order as $data){
         //     foreach($data->OrderDetail as $item){
-        //         return $order;
+                // return $order;
         //     }
         // }
 
